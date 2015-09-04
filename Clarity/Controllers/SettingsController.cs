2137f40863a978173386baaf13174fe4a0459275
Emily Huang <ehuang2010@gmail.com>
2015-09-03 21:30:26 -0700

    Added settings

---
+++ Clarity/Controllers/SettingsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Clarity.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public void Get()
+        {
+            string currJSON = "";
+            string filePath = Server.MapPath("~/Shared/settings.json");
+            
+            Response.ContentType = "application/json";
+
+            try
+            {
+                if (System.IO.File.Exists(filePath))
+                {
+                    StreamReader reader = new StreamReader(filePath);
+
+                    using (reader)
+                    {
+                        currJSON = reader.ReadToEnd();
+                    }
+                }
+
+                if (currJSON == "")
+                {
+                    currJSON = "{error: \"Missing settings.json file.\"}";
+                }
+                
+                Response.Write(currJSON);
+            }
+            catch (Exception e)
+            {
+                currJSON = "{error: \"Error occurred while loading settings JSON: " + e.Message + "\"}";
+                Response.Write(currJSON);
+            }
+        }
+    }
+}
---
+++ Clarity/Scripts/main.js
@@ -0,0 +1,59 @@
+﻿var settingsBtn = $("#settingsBtn");
+var settings = $("#settings");
+var errorLB = $("#error");
+
+var settingsObj;
+
+settingsBtn.click(function () {
+    getSettings();
+    mapSettings();
+});
+
+function getSettings() {
+    $.get("../Settings/Get", function (data) {
+        settingsObj = data;
+    });
+}
+
+function mapSettings() {
+    var tmpValue = null;
+
+    if (settingsObj["error"] != null) {
+        errorLB.text(settingsObj["error"]);
+        errorLB.show();
+        return;
+    }
+
+    tmpValue = settingsObj["categoryID"];
+    if (tmpValue != null && tmpValue > 0) $("#category option:eq(" + tmpValue + ")").prop('selected', true);
+
+    tmpValue = settingsObj["clarityReward"];
+    if (tmpValue != null) {
+        $("#clarity-reward").val(tmpValue);
+    } else {
+        $("#clarity-reward").val(0.05);
+    }
+
+    tmpValue = settingsObj["numTrials"];
+    if (tmpValue != null) {
+        $("#num-trials").val(tmpValue);
+    } else {
+        $("#num-trials").val(4);
+    }
+
+    tmpValue = settingsObj["trialDuration"];
+    if (tmpValue != null) {
+        $("#trial-duration").val(tmpValue);
+    } else {
+        $("#trial-duration").val(60);
+    }
+
+    for (var i = 0; i < settingsObj.components.length; i++) {
+        tmpValue = settingsObj.components[i];
+        if (tmpValue != null) {
+            $("#clarity-" + (i + 1)).val(tmpValue);
+        }      
+    }
+
+    settings.show();
+}
