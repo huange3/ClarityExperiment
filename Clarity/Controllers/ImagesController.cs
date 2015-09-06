f1a749796821120408318baa66a9b772f670c3af
Emily Huang <ehuang2010@gmail.com>
2015-09-05 22:35:53 -0700

    Progress

---
+++ Clarity/Controllers/ImagesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Script.Serialization;
+using Clarity.Shared;
+
+namespace Clarity.Controllers
+{
+    public class ImagesController : Controller
+    {
+        // GET: Images
+        public void Load(int catID)
+        {
+            string[] imageFiles;
+            string filePath = "";
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            try
+            {
+                if (catID <= 0) catID = (int)Constants.Folders.Animals;
+
+                filePath = "~/Images/" + Enum.GetName(typeof(Constants.Folders), catID);
+
+                imageFiles = Directory.GetFiles(filePath);
+
+                Response.ContentType = "application/json";
+                Response.Write(js.Serialize(imageFiles));
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 500;
+                Response.Write("Error occurred while loading images: " + e.Message);
+            }
+            finally
+            {
+                imageFiles = null;
+                js = null;
+            }
+        }
+    }
+}
--- Clarity/Controllers/SettingsController.cs
+++ Clarity/Controllers/SettingsController.cs
@@ -4,43 +4,94 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using Clarity.Shared;
 
 namespace Clarity.Controllers
 {
     public class SettingsController : Controller
     {
         // GET: Settings
-        public void Get()
+        public void Load()
         {
             string currJSON = "";
-            string filePath = Server.MapPath("~/Shared/settings.json");
-            
-            Response.ContentType = "application/json";
+            string filePath = Constants.filePathSettings;
 
             try
             {
                 if (System.IO.File.Exists(filePath))
                 {
                     StreamReader reader = new StreamReader(filePath);
-
                     using (reader)
                     {
                         currJSON = reader.ReadToEnd();
                     }
                 }
 
-                if (currJSON == "")
-                {
-                    currJSON = "{error: \"Missing settings.json file.\"}";
-                }
-                
+                if (currJSON == "") currJSON = "{error: \"Missing settings.json file.\"}";
+
+                Response.ContentType = "application/json";
                 Response.Write(currJSON);
             }
             catch (Exception e)
             {
                 currJSON = "{error: \"Error occurred while loading settings JSON: " + e.Message + "\"}";
+                Response.ContentType = "application/json";
                 Response.Write(currJSON);
             }
+        }
+
+        public void Save()
+        {
+            var currJSON = "";
+            var filePath = Server.MapPath(Constants.filePathSettings);
+            var returnVal = "";
+            StreamWriter writer = null;
+
+            try
+            {
+                currJSON = readRequestStream();
+
+                if (currJSON != "")
+                {
+                    writer = new StreamWriter(filePath, false);
+
+                    using (writer)
+                    {
+                        writer.WriteLine(currJSON);
+                    }
+
+                    returnVal = "Settings saved successfully!";
+                }
+                else
+                {
+                    returnVal = "Invalid JSON received. Please try again.";
+                }
+
+                Response.Write(returnVal);
+            }
+            catch (Exception e)
+            {
+                returnVal = "Error occurred while saving settings JSON: " + e.Message;
+                Response.Write(returnVal);
+            }
+            finally
+            {
+                writer = null;
+            }
+        }
+
+        public string readRequestStream()
+        {
+            var currData = "";
+
+            if (Request.ContentLength > 0)
+            {
+                using (StreamReader reader = new StreamReader(Request.InputStream))
+                {
+                    currData = reader.ReadToEnd();
+                }
+            }
+            return currData;
         }
     }
 }
