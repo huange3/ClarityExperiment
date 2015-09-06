96ca59c1d66264cb178b36a08c7b3a684f4fd0c4
Emily Huang <ehuang2010@gmail.com>
2015-09-05 22:38:49 -0700

    Moved folders

---
+++ Clarity/Shared/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clarity.Shared
+{
+    public static class Constants
+    {
+        public const string filePathSettings = "../Shared/settings.json";
+        public const string filePathImages = "../Images";
+
+        public enum Folders
+        {
+            Animals = 1,
+            RealEstate = 2,
+            Sports = 3,
+            Nature = 4,
+            FunnyQuotes = 5,
+            Celebrities = 6,
+            Food = 7
+        }
+    }
+}
